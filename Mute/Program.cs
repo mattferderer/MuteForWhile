4a8683902692afbc589c5a4ce10d6a60d217e9b2
Matt Ferderer <contact@mattferderer.com>
2019-03-19 14:56:12 -0500

    Init

---
+++ Mute/Program.cs
@@ -0,0 +1,19 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Threading;
+
+namespace Mute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+            defaultPlaybackDevice.Mute(true);
+            Console.WriteLine($"Muted: {defaultPlaybackDevice.IsMuted}");
+            Thread.Sleep(60000);
+            defaultPlaybackDevice.Mute(false);
+            Console.WriteLine($"Muted: {defaultPlaybackDevice.IsMuted}");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Purpose
+
+This is a simple app to mute your speakers for a specific amount of time. Adjust this line `Thread.Sleep(60000);` to change the time. The time is in milliseconds.
+
+This runs on Windows.
+
+## Reason
+
+I often stop or mute music when someone visits me or mute during an annoying & very loud commercial. I tend to forget to unmute the music. This solves that.
